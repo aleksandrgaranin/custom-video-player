4e61aadd15fe5255ab221612261d225b1b03c10b
alek <aleksandrgaranin89@gmail.com>
2022-11-21 15:18:19 -0800

    Mini Player, Full screen, Theater mode functionalities

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
 const playPouseBtn = document.querySelector('.play-pause-btn')
 const video = document.querySelector("video")
 const videoContainer = document.querySelector(".video-container")
+const theaterBtn = document.querySelector('.theater-btn')
+const fullScreenBtn = document.querySelector('.full-screen-btn')
+const miniPlayerBtn = document.querySelector('.mini-player-btn')
 
-playPouseBtn.addEventListener('click', togglePlay)
+
 
 document.addEventListener("keydown", e => {
+    const tagName = document.activeElement.tagName.toLowerCase()
+    if (tagName === 'input') return
     switch (e.key.toLowerCase()) {
         case " ":
+            if (tagName === 'button') return
         case "k":
             togglePlay()
+            break;
+        case 'f':
+            toggleFullScreen()
             break
+        case 't':
+            toggleTheater()
+            break
+        case 'i':
+            toggleMiniPlayer()
+            break
+
     }
 })
+// Theater
+
+theaterBtn.addEventListener('click', toggleTheater)
+
+function toggleTheater() {
+    videoContainer.classList.toggle('theater')
+}
+
+// Mini Player 
+miniPlayerBtn.addEventListener('click', toggleMiniPlayer)
+
+function toggleMiniPlayer() {
+    if (videoContainer.classList.contains('mini-player')) {
+        document.exitPictureInPicture()
+    } else {
+        video.requestPictureInPicture()
+    }
+}
+
+video.addEventListener("enterpictureinpicture", () => [
+    videoContainer.classList.add('mini-player')
+])
+
+video.addEventListener("leavepictureinpicture", () => [
+    videoContainer.classList.remove('mini-player')
+])
+// Full Screen
+
+fullScreenBtn.addEventListener('click', toggleFullScreen)
+
+function toggleFullScreen() {
+    if (document.fullscreenElement == null) {
+        videoContainer.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+}
+
+document.addEventListener('fullscreenchange', () => {
+    videoContainer.classList.toggle('full-screen', document.fullscreenElement)
+})
+
+// Play/Pause
+
+playPouseBtn.addEventListener('click', togglePlay)
+video.addEventListener("click", togglePlay)
 
 function togglePlay() {
     video.paused ? video.play() : video.pause()
@@ -24,3 +86,4 @@ video.addEventListener("play", () => {
 video.addEventListener("pause", () => {
     videoContainer.classList.add("paused")
 })
+
