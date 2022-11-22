16ec45223ccf236dd1dd221c06b84ffcad4e01fa
alek <aleksandrgaranin89@gmail.com>
2022-11-21 18:13:58 -0800

    Volume and Duration functionalities

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const videoContainer = document.querySelector(".video-container")
 const theaterBtn = document.querySelector('.theater-btn')
 const fullScreenBtn = document.querySelector('.full-screen-btn')
 const miniPlayerBtn = document.querySelector('.mini-player-btn')
+const muteBtn = document.querySelector('.mute-btn')
+const volumeSlider = document.querySelector('.volume-slider')
+const currentTime = document.querySelector(".current-time")
+const totalTime = document.querySelector(".total-time")
 
 
 
@@ -25,9 +29,75 @@ document.addEventListener("keydown", e => {
         case 'i':
             toggleMiniPlayer()
             break
-
+        case 'm':
+            toggleMute()
+            break
+        case "arrowleft":
+        case "j":
+            skip(-5)
+            break
+            case "arrowright":
+        case "r":
+            skip(5)
+            break
     }
 })
+
+// Duration
+video.addEventListener('loadeddata', () => {
+    totalTime.textContent = formatDuration(video.duration)
+})
+
+video.addEventListener("timeupdate", () => {
+    currentTime.textContent = formatDuration(video.currentTime)
+})
+
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+    minimumIntegerDigits: 2,
+})
+
+function formatDuration(time) {
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time / 60) % 60
+    const hours = Math.floor(time / 3600)
+    if (hours === 0) {
+        return `${minutes}:${leadingZeroFormatter.format(seconds)}`
+    } else {
+        return `${hours}:${leadingZeroFormatter.format(minutes)}:${leadingZeroFormatter.format(seconds)}`
+    }
+
+}
+
+function skip(duration) {
+    video.currentTime += duration
+}
+
+// Volume
+muteBtn.addEventListener('click', toggleMute)
+
+volumeSlider.addEventListener('input', e => {
+    video.volume = e.target.value
+    video.muted = e.target.value === 0
+})
+
+function toggleMute() {
+    video.muted = !video.muted
+}
+
+video.addEventListener('volumechange', () => {
+    volumeSlider.value = video.volume
+    let volumeLevel
+    if (video.muted || video.volume === 0) {
+        volumeSlider.value = 0
+        volumeLevel = "muted"
+    } else if (video.volume >= .5) {
+        volumeLevel = "high"
+    } else {
+        volumeLevel = "low"
+    }
+    videoContainer.dataset.volumeLevel = volumeLevel
+})
+
 // Theater
 
 theaterBtn.addEventListener('click', toggleTheater)
